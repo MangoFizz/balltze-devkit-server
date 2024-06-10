61225ea0c6c76ab80de45f93c0a6952f3112e53b
MangoFizz <ccs81.mr@gmail.com>
2024-06-10 00:46:52 -0600

    Object list function

--- lua/modules/devkit/functions.lua
+++ lua/modules/devkit/functions.lua
@@ -2,6 +2,16 @@
 
 Devkit = {}
 
+local metaobject = require "devkit.metaobject"
+
+local function addSignToIndex(value)
+    if value == 65535 then
+        return -1
+    else
+        return value
+    end
+end
+
 function Devkit.getTagList()
     local tagCount = Engine.tag.getTagDataHeader().tagCount
     local tagList = {}
@@ -11,4 +21,39 @@ function Devkit.getTagList()
         table.insert(tagList, { path = tag.path, handle = tag.handle.value, class = class})
     end
     return tagList
+end
+
+function table.find(t, f)
+    for k, v in pairs(t) do
+        if f(v, k) then
+            return v
+        end
+    end
+end
+
+function Devkit.getObjectList()
+    local objects = {}
+    local scenario = Engine.tag.getTag(0)
+    assert(scenario ~= nil, "Scenario tag not found")
+    ---@type MetaEngineTagDataScenario
+    local scenarioData = scenario.data 
+    local objectNames = scenarioData.objectNames
+
+    for i = 0, 2047 do
+        local object = Engine.gameState.getObject(i)
+        if object ~= nil then
+            local scenarioObjectName = {}
+            if addSignToIndex(object.nameListIndex) ~= -1 then
+                scenarioObjectName = table.find(objectNames.elements, function (v, k) return k == object.nameListIndex + 1 end)
+            end
+            table.insert(objects, { 
+                index = i, 
+                tagHandle = object.tagHandle.value, 
+                objectType = metaobject.enumUserdataToString(object.type), 
+                parentIndex = addSignToIndex(object.parentObject.index),
+                name = scenarioObjectName.name
+            })
+        end
+    end
+    return objects
 end
